8be7e38484ed6cdf9fb7e93fc8289cfcdc100340
fmbahrt <lilbahrt@gmail.com>
2023-05-30 13:05:17 +0200

    more synch stuff

--- .github/synchronize_images.py
+++ .github/synchronize_images.py
@@ -1,44 +1,110 @@
+import argparse
+import json
+
+from pathlib import Path
 from subprocess import run
 
 
-#run(["helm", "dep", "update", "cluster-addons/argocd/prod"], check=True)
+def initialize_helm(root):
+    values_file = root / "values.yaml"
+    assert values_file.is_file(), f"{values_file} is not a file."
+    # Start by updating dependencies - we need that for rendering
+    run(["helm", "dep", "update", root], check=True)
+    
 
-results = run(["helm", "template", "cluster-addons/argocd/prod", "-f", "cluster-addons/argocd/prod/values.yaml"], capture_output=True, text=True).stdout
-
-rendered_images = set([])
-
-for line in iter(results.splitlines()):
-    if "image:" in line:
-        # Sanitize output
-        image = line
-        image = image.replace("image:", "")
-        image = image.replace('''"''', "")
-        image = image.strip()
-        rendered_images.add(image)
-
-docker_map = {
-    'my.registry/argoproj/workflow-controller:v3.4.7': 'quay.io/argoproj/workflow-controller:v3.4.7',
-    'my.registry/argoproj/argocd:v1.7.2': 'quay.io/argoproj/argocd:v1.7.2',
-    'my.registry/argoproj/argocli:v3.4.7': 'quay.io/argoproj/argocli:v3.4.7',
-    'my.registry/argoproj/argocd:v2.7.2': 'quay.io/argoproj/argocd:v2.7.2',
-    'my.registry/redis:7.0.11-alpine': 'public.ecr.aws/docker/library/redis:7.0.11-alpine',
-}
-
-# Check if declared docker images covers the image rendered in Helm
-declared_public_images = set(docker_map.keys())
-
-image_diff = rendered_images - declared_public_images
-
-# If this breaks - please revisit your Helm/Kustomize configurations and check if every possible image spec has been set.
-# assert len(image_diff) == 0, f"Declared images does not cover the rendered images. Got diff {image_diff}."
+def initialize_engine(engine, root):
+    if engine == "helm":
+        initialize_helm(root)
+    else:
+        raise NotImplementedError("Engine not implemented")
 
 
-# Pull, Retag and publish
-for private, public in docker_map.items():
-    # First we check if we try to sync an image that already exists in our target repository
-    #run(["docker", "manifest", "inspect", private], check=True)
-    print(f"docker manifest inspect {private}")
-    run(["docker", "pull", public], check=True)
-    run(["docker", "tag", public, private], check=True)
-    #run(["docker", "push", private], check=True)
-    print(f"docker push {private}")
+def fetch_images_helm(root):
+    results = run(["helm", "template", root, "-f", root / "values.yaml"], capture_output=True, text=True).stdout
+    
+    rendered_images = set([])
+    
+    for line in iter(results.splitlines()):
+        if "image:" in line:
+            # Sanitize output
+            image = line
+            image = image.replace("image:", "")
+            image = image.replace('''"''', "")
+            image = image.strip()
+            rendered_images.add(image)
+
+    return rendered_images
+
+
+def fetch_images(engine, root):
+    if engine == "helm":
+        return fetch_images_helm(root)
+    else:
+        raise NotImplementedError("Engine not implemented")
+
+
+def synchronize_images(image_mappings):
+    for private, public in image_mappings.items():
+        # First we check if we try to sync an image that already exists in our target repository
+        #run(["docker", "manifest", "inspect", private], check=True)
+        print(f"docker manifest inspect {private}")
+        run(["docker", "pull", public], check=True)
+        run(["docker", "tag", public, private], check=True)
+        #run(["docker", "push", private], check=True)
+        print(f"docker push {private}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Synchronize Docker images between registries.",
+        epilog="Maintained by devops@supwiz.com",
+    )
+
+    parser.add_argument( # Project root dir
+        "root",
+        type=str,
+        help="Root dir of the deployment",
+        nargs=1,
+    )
+    parser.add_argument(
+        "--engine",
+        default="helm",
+        choices=["helm"],
+        help="Which rendering engine to use.",
+    )
+    parser.add_argument(
+        "--check-declared-images",
+        action="store_true",
+        help="check if the declared images covers the rendered images",
+    )
+    parser.add_argument(
+        "--synchronize",
+        action="store_true",
+        help="Synchronize images between registries",
+    )
+
+    args = parser.parse_args()
+
+    root_path = Path(args.root[0])
+    assert root_path.is_dir()
+
+    image_mappings_path = root_path/ "workflow_docker_mappings.yaml"
+    with open(image_mappings_path) as json_file:
+        image_mappings = json.load(json_file)
+        print(image_mappings)
+
+    # Assert missing images in mappings
+    if args.check_declared_images:
+
+        # Start by initializing the engine
+        initialize_engine(args.engine, root_path)
+
+        # Fetch all rendered images
+        rendered_images = fetch_images(args.engine, root_path) 
+
+        image_diff = rendered_images - declared_public_images 
+        assert len(image_diff) == 0, f"Declared images does not cover the rendered images. Got diff {image_diff}."
+
+    # Synchronize images
+    if args.synchronize:
+        synchronize_images(image_mappings)
